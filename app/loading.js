f1360656b4ba3b305b52be06afad8405732a5a7b
kotreshdm <kotreshdm@gmail.com>
2024-10-02 07:33:28 +0530

    initial commit

--- app/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className='d-flex justify-content-center align-items-center vh-90 text-danger display-1 fw-bold'>
-      LOADING
-    </div>
-  );
-}
--- app/not-found.js
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className='d-flex justify-content-center align-items-center vh-90 text-danger display-1 fw-bold'>
-      404 | Page Not Found
-    </div>
-  );
-}
