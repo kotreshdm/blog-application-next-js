707bf09d965e2100973ebcaf97468de34ee2fbc9
kotreshdm <kotreshdm@gmail.com>
2024-10-10 21:45:41 +0530

    login aded

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <div >
-      <h1>Sakshi K</h1>    
+    <div>
+      <h1>Sakshi K</h1>
+      <Link href='/login'>Login</Link>
     </div>
   );
 }
