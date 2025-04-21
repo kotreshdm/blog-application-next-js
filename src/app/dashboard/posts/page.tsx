46cfc92aff5ff8f182868fe30b0902b76fcce0b2
kotreshdm <kotreshdm@gmail.com>
2025-04-21 22:26:01 +0530

    Type erroe fixed

--- src/app/dashboard/posts/page.tsx
+++ src/app/dashboard/posts/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 import React from "react";
 import Pageheader from "../components/page-header/PageHeader";
-import { useDashboardContext } from "@/utils/context/DashboardContext";
 
 export default function page() {
-  const { fetchPosts } = useDashboardContext();
-
   return (
     <div className='p-6'>
-      <Pageheader title='Posts' onRefresh={fetchPosts} />
+      <Pageheader title='Posts' onRefresh={() => {}} />
     </div>
   );
 }
